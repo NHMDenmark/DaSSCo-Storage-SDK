ea8b83941e64904e83316da3685aa2949dd966a7
Lars Dam <larsphdam@gmail.com>
2025-08-01 10:19:21 +0200

    update fileproxy test

--- tests/test_fileproxy.py
+++ tests/test_fileproxy.py
@@ -40,13 +40,13 @@ def setup_and_teardown():
     yield
 
     # after
-    """
+    
     try:
         client.file_proxy.delete_share(INSTITUTION_NAME, COLLECTION_NAME, ASSET_GUID, ["Test user"], 1)
         client.assets.delete_metadata(ASSET_GUID)
     except Exception as e:
         print(f"Failed to clean up: {e}")
-    """
+    
         
 @pytest.mark.order(1)
 def test_delete_share():
@@ -62,21 +62,21 @@ def test_open_share():
 
 @pytest.mark.order(3)
 def test_upload_file():
-    """
+    
     res = client.file_proxy.upload(FILE_NAME, INSTITUTION_NAME, COLLECTION_NAME, ASSET_GUID, 1)
     
     assert res.status_code == 200
     upload_response = res.json()
     print(upload_response)
-    """
+    
 
 
 @pytest.mark.order(4)
 def test_get_file():
-    res = client.file_proxy.upload(FILE_NAME, INSTITUTION_NAME, COLLECTION_NAME, ASSET_GUID, 1)    
+    """res = client.file_proxy.upload(FILE_NAME, INSTITUTION_NAME, COLLECTION_NAME, ASSET_GUID, 1)    
     assert res.status_code == 200
     upload_response = res.json()
-    print(upload_response)
+    print(upload_response)"""
 
     resp = client.file_proxy.list_available_files(ASSET_GUID)
     print(resp.content)
@@ -99,9 +99,8 @@ def test_list_file_info():
     res = client.file_proxy.list_file_info(ASSET_GUID)
     assert res.status_code == 200
 
-"""
+
 @pytest.mark.order(7)
 def test_delete_file():
     res = client.file_proxy.delete_file(INSTITUTION_NAME, COLLECTION_NAME, ASSET_GUID, FILE_NAME)
     assert res.status_code == 204
-"""
